2b6dff67e9551536429a8947cc62bf78b18c3d14
Scott Walter <sjwalter@gmail.com>
2024-03-07 02:01:04 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ cd $FONT_DIR
 
 echo "Installing zinit ..."
 setopt promptsubst
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+bash -c " yes | $(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # echo "Adding ZSH syntax highlighting ..."
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
