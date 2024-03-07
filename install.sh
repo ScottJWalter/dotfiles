80aa857ee90a5fecc1a85371b3d4fa33f4fc774a
Scott Walter <sjwalter@gmail.com>
2024-03-06 23:10:50 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,9 +33,14 @@ cd $FONT_DIR
 # Create a shortcut to the OhMyZSH custom folder
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
+echo "Installing zinit ..."
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+echo "Adding ZSH syntax highlighting ..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
 echo "Adding ZSH plugins ..."
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone --depth=1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 
